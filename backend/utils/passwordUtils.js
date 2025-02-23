8b1c2c33220f5882e2178bb0f160d94121d89381
Arenz Papas <rnzpapas@gmail.com>
2025-02-23 22:36:54 +0800

    Re-confgured frontend & backend

--- backend/utils/passwordUtils.js
+++ backend/utils/passwordUtils.js
@@ -1,9 +1,9 @@
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 const salt = 10;
 
 export const hashPassword = (password) => {
     try{
-        const hashedPassword = bcrypt.hashSync(password, salt);
+        const hashedPassword = bcryptjs.hashSync(password, salt);
         return hashedPassword;
     }catch (err) {
         throw err;
@@ -12,7 +12,7 @@ export const hashPassword = (password) => {
 
 export const comparePassword = async (password, dbpassword) => {
     try{
-        const result = await bcrypt.compare(password, dbpassword);
+        const result = await bcryptjs.compare(password, dbpassword);
         return result;
     }catch(err){
         return false;
