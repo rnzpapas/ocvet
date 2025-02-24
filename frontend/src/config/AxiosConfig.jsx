5b65287301eb900b48bf2ad8b32957ef371397df
Arenz Papas <rnzpapas@gmail.com>
2025-02-24 15:30:41 +0800

    created and inserted axiosInstance.

--- frontend/src/config/AxiosConfig.jsx
+++ frontend/src/config/AxiosConfig.jsx
@@ -1,9 +1,9 @@
-import axiosInstance from 'axiosInstance'
+import axios from 'axios'
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001";
 
-const axiosInstanceInstance = axiosInstance.create({
+const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
-export default axiosInstanceInstance;
+export default axiosInstance;
