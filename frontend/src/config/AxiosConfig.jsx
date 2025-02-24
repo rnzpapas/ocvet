4066b44244787f0d00c693bc8c41a7c619229fcf
Arenz Papas <rnzpapas@gmail.com>
2025-02-24 15:08:39 +0800

    .env backend test.

--- frontend/src/config/AxiosConfig.jsx
+++ frontend/src/config/AxiosConfig.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001";
+
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: { "Content-Type": "application/json" },
