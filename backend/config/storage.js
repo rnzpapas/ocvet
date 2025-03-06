0cc6ebb3e7c3f112c34e948a262228945133efe9
Arenz Papas <rnzpapas@gmail.com>
2025-03-06 23:44:08 +0800

    AWS library import update.

--- backend/config/storage.js
+++ backend/config/storage.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import multerS3 from 'multer-s3';
-import { S3Client } from 'aws-sdk/client-s3';
+import { S3Client } from '@aws-sdk/client-s3';
 import path from 'path';
 require("dotenv").config();
 
