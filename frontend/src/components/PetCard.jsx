c0388bdf383d92dbc46c16972992ca0e3454fc62
Arenz Papas <132368840+rnzpapas@users.noreply.github.com>
2025-05-06 13:21:48 +0800

    Pet card no img.

--- frontend/src/components/PetCard.jsx
+++ frontend/src/components/PetCard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import NoImg from '@/assets/noimg.png';
 
 function PetCard({petName, img = "", style, onClickFunc, id}) {
   let imgDirSrc = import.meta.env.VITE_AWS_BUCKET_CONNECTION;
@@ -9,7 +10,11 @@ function PetCard({petName, img = "", style, onClickFunc, id}) {
   return (
     <section className="w-[80px] h-[100px] xl:w-[140px] xl:h-[150px] cursor-pointer relative" onClick={onClickFunc} id={id}>
         <section className=" h-[80%]">
-          <img src={`${imgDirSrc}/pet/${imgSrc}`} alt="as" className="h-full w-full object-cover rounded-tl-lg rounded-tr-lg" />
+          <img 
+            src={`${imgSrc ? `${imgDirSrc}/pet/${imgSrc}` : NoImg}`}
+            alt={`${imgSrc ? imgSrc : "No Image"}`}
+            className="h-full w-full object-cover rounded-tl-lg rounded-tr-lg" 
+          />
         </section>
         <section className="bg-raisin-black h-[20%] flex items-center justify-center w-full rounded-bl-lg rounded-br-lg ">
             <p className="text-white-smoke font-lato font-semibold text-content-xtrasm"> {petName} </p>
