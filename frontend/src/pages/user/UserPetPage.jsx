d0bebc2bf5a74cc31c3ea217cb367bec507db9d3
Arenz Papas <132368840+rnzpapas@users.noreply.github.com>
2025-05-06 13:41:50 +0800

    Pet registration button.

--- frontend/src/pages/user/UserPetPage.jsx
+++ frontend/src/pages/user/UserPetPage.jsx
@@ -171,11 +171,11 @@ function UserPetPage() {
                                     : 
                                     <h5> No pets found. </h5>
                         }
-                        <div className="flex flex-col gap-2 items-center">
-                            <h5 className="font-instrument-sans font-semibold uppercase text-content-sm ">Register Your Pet(s)</h5>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-[50px] mt-5 lg:w-[60px] lg:mt-3 xl:w-[80px] fill-silver hover:fill-raisin-black">
+                        <div className="flex flex-col gap-1.5 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-[50px lg:w-[60px] xl:w-[80px] fill-silver hover:fill-raisin-black">
                                 <path d="M64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l320 0c8.8 0 16-7.2 16-16l0-320c0-8.8-7.2-16-16-16L64 80zM0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM200 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/>
                             </svg>
+                            <h5 className="font-instrument-sans font-semibold uppercase text-content-sm ">Register Your Pet(s)</h5>
                         </div>
                     </section>
                 </section>
